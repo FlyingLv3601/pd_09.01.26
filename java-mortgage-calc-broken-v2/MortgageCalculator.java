3be1b588ff50d600809cdfa3bc399de0b215d2c3
unknown <deivis.stasuls@gmail.com>
2026-01-09 10:52:18 +0200

    YEEEEEEEEES

--- java-mortgage-calc-broken-v2/MortgageCalculator.java
+++ java-mortgage-calc-broken-v2/MortgageCalculator.java
@@ -6,27 +6,27 @@ public class MortgageCalculator {
     public final static byte MONTH_IN_YEAR = 12;
     public final static byte PERCENT = 100;
 
-    public MortgageCalculator(credit, yearlyPercentageRate, years) {
+    public MortgageCalculator(int credit, int yearlyPercentageRate, int years) {
         this.credit = credit;
         this.yearlyPercentageRate = yearlyPercentageRate;
         this.years = years;
     }
 
     private int getMonths() {
-        this.years *= MONTH_IN_YEAR;
+        return this.years *= MONTH_IN_YEAR;
     }
 
     private float getMonthlyRate() {
-        this.yearlyPercentageRate /= PERCENT / MONTH_IN_YEAR;
+        return this.yearlyPercentageRate /= PERCENT / MONTH_IN_YEAR;
     }
 
     private double getFutureValueInterestFactor() {
-        Math.pow((1 + getMonthlyRate()), getMonths());
+        return Math.pow((1 + getMonthlyRate()), getMonths());
     }
 
     // https://www.luminor.lv/lv/privatpersonam/hipotekara-kredita-kalkulators
     public double calculate() {
         double monthlyMortgagePayment = this.credit * (getMonthlyRate() * getFutureValueInterestFactor()) / (getFutureValueInterestFactor() - 1);
-        Math.round(monthlyMortgagePayment * 100d) / 100d; // two digits after the decimal point
+        return Math.round(monthlyMortgagePayment * 100d) / 100d; // two digits after the decimal point
     }
 }
